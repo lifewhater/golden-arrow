d56b08504b10db651d22c28633588bafc47dfad4
cherrychaddha <cherrychaddha@gmail.com>
2025-09-05 03:04:18 -0700

    Working on Gallery view, need to add more elements and route them

--- golden-arrow/src/pages/gallery.vue
+++ golden-arrow/src/pages/gallery.vue
@@ -1,24 +1,40 @@
 <template>
+  <section class="">
 
-  <section>
-    <!-- IMAGES LEFT SIDE-->
-    <div class="flex flex-col h-full w-1/2 z-0">
-      <div v-for="(img, i) in product?.images" :key="i" class="relative w-full h-full ">
-        <img ref="images" :src="img" :alt="product?.name" class="object-cover aspect-[5/6] h-full w-full">
+    <!-- DESKTOP VIEW-->
+    <div class="hidden md:block md:flex-col md:h-full md:w-1/2 md:z-0">
+      <div v-for="(img, i) in product?.images" :key="i" class="relative w-full h-full">
+        <img ref="desktop" :src="img" :alt="product?.name" class="object-cover aspect-[5/6] h-full w-full"
+          loading="lazy">
       </div>
     </div>
 
-    <!-- TEXT RIGHT SIDE-->
-    <div ref="aside" class="h-screen w-1/2 fixed top-0 right-0 z-50 m-[var(--ga-margin-left)]">
-      <div class="z-50font flex flex-col gap-[var(--ga-margin-Card)]
-      ml-[var(--ga-label-left)] mt-[var(--ga-label-margin)]">
+    <!-- MOBILE VIEW -->
+    <div class="md:hidden  w-full flex flex-row
+          h-[var(--ga-card-height)] mt-30
+          overflow-x-auto snap-x snap-mandatory scroll-smooth">
+      <div v-for="(img, i) in product?.images" :key="i" class="shrink-0 w-screen snap-start">
+        <img ref="mobile" :src="img" :alt="product?.name" class="object-cover aspect-[5/6] h-full w-full"
+          loading="lazy">
+      </div>
+    </div>
+
+    <!-- TEXT -->
+    <div class="md:fixed md:ml-[var(--ga-margin-left)] md:w-1/2 
+    h-auto w-full top-0 right-0 z-50">
+
+      <div class="z-50 flex flex-col gap-[var(--ga-margin-Card)] mx-[var(--ga-margin-leftSm)]
+        mt-[var(--ga-label-top)]
+        md:ml-[var(--ga-label-left)] md:mt-[var(--ga-label-margin)] 
+        md:gap-[var(--ga-margin-Card)]">
 
         <!-- NAME AND PRICE -->
         <div class="flex flex-row justify-between">
           <div class="text-5xl text-white font">
             {{ product?.name }}
           </div>
-          <div class="text-5xl text-white font">
+
+          <div class="text-5xl text-white font md:mr-[var(--ga-margin-left)]">
             ${{ product?.price }}
           </div>
         </div>
@@ -29,6 +45,7 @@
         </div>
       </div>
     </div>
+
   </section>
 </template>
 
@@ -42,8 +59,7 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger)
 
 
-const images = ref([])
-const aside = ref(null)
+const desktop = ref([])
 
 const store = useProductStore()
 const route = useRoute()
@@ -65,7 +81,7 @@ onMounted(async () => {
 
   //GSAP Animation
 
-  images.value.forEach((element) => {
+  desktop.value.forEach((element) => {
     gsap.from(element, {
       scrollTrigger: {
         trigger: element,
